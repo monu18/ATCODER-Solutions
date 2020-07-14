10302d8882697e8636c6de533c718042b0321d08
monu <mozis.bot@gmail.com>
2020-07-14 15:27:38 +0530

    Atom

---
+++ abc126/abc126A.cpp
@@ -0,0 +1,71 @@
+//mOzis_
+/////*31022618*/////
+//****//MONU KUMAR\****//
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define ull unsigned long long
+#define lp(i,l,r) for (long long int i = l; i < r; i++)
+#define mem(a,x) memset(a,x,sizeof(a))              //set elements of array to some value
+#define pi 3.1415926535897932384626
+#define mod 1000000007
+#define pb(x) push_back(x)
+#define ff first
+#define ss second
+#define siz 256                        //characters are stored using 8 bit and there can be
+bool anagram(char *s1, char *s2)     //256 possible characters only in lowercase alphabet
+{
+	ll count[siz];
+    mem(count,0) ;
+    ll i;
+    for (i = 0; s1[i] && s2[i];  i++)
+    {
+        count[s1[i]]++;
+        count[s2[i]]--;
+    }
+    if (s1[i] || s2[i])
+    {
+      return false;
+    }
+    lp(i,0,siz)
+    {
+    	 if (count[i])
+    	 {
+    	 	 return false;
+    	 }
+    }
+     return true;
+
+}
+int main()
+{
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+	ll n,k;
+	cin>>n>>k;
+	string s;
+	cin>>s;
+	for (int i = 0; i < n; ++i)
+	{
+		if (i==(k-1))
+		{
+			if (s[i]=='A')
+			{
+				s[i]='a';
+			}
+			if (s[i]=='B')
+			{
+				s[i]='b';
+			}
+			if (s[i]=='C')
+			{
+				s[i]='c';
+			}
+		}
+		cout<<s[i];
+	}
+
+	return 0;
+}
+//////////*********************end of program*********************//////////
